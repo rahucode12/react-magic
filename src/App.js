3c301c4cf31e5859a4999a9757e55b3bc0981288
Rahul Giri <rahucode1@gmail.com>
2022-10-17 11:22:44 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import "./styles.css";
 import React from 'react'
 import DebounceUtil from "./components/debounce-util";
-
+import QrCode from "./components/qr-code"
 export default function App() {
 
   return (
     <>
       <div className="App">
         <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <h2>Start editingg to see some magic happen!</h2>
       </div>
       <DebounceUtil className="Center" />
+      <QrCode />
     </>
   );
 }
